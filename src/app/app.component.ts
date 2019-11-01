171a9a0e3505f67e50bfdaf79b91087c6a5803e4
Radhey Shyam <Radhey.Shyam@srmtechsol.com>
2019-11-01 06:42:42 -0400

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-demo';
+  update:boolean=true;
+
+  constructor(public updates : SwUpdate){
+    updates.available.subscribe( event => {
+      this.update = true;
+
+      // updates.activateUpdate().then(() => {
+      //   document.location.reload();
+      // })
+      
+    })
+  }
+
 }
